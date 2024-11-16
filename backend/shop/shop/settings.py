372bcdc86885450d8b00fde1c0ec7eeb38ac123e
Artyom Kotovskiy <mrartem1927@gmail.com>
2024-11-15 20:45:19 -0500

    Socket timeouts to pool retries
    Changed socket connection timeouts to pool retries
    because the timeouts didnt seem to work

--- backend/shop/shop/settings.py
+++ backend/shop/shop/settings.py
@@ -199,26 +199,29 @@ KAFKA_CONFIG = {
 
 EXCLUDE_FROM_ADMIN = ['contenttypes', 'admin', 'sessions']
 
-# CACHES = {
-#     "default": {
-#         "BACKEND": "django_redis.cache.RedisCache",
-#         "LOCATION": "redis://redis-server:6379/1",
-#         "OPTIONS": {
-#             "CLIENT_CLASS": "django_redis.client.DefaultClient"
-#         }
-#     }
-# }
-
-
 CACHES = {
     "default": {
         "BACKEND": "django_redis.cache.RedisCache",
-        "LOCATION": "redis://127.0.0.1:6379/1",
+        "LOCATION": "redis://redis-server:6379/1",
         "OPTIONS": {
-            'CLIENT_CLASS': 'django_redis.client.DefaultClient',
-            'SOCKET_CONNECT_TIMEOUT': 0.1,  # 100ms connect timeout
-            'SOCKET_TIMEOUT': 0.1,  # 100ms operation timeout
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "CONNECTION_POOL_KWARGS": {
+                "retry_attempts": 3,
+            }
         }
-    },
-
+    }
 }
+
+
+# CACHES = {
+#     "default": {
+#         "BACKEND": "django_redis.cache.RedisCache",
+#         "LOCATION": "redis://127.0.0.1:6379/1",
+#         "OPTIONS": {
+#             "CLIENT_CLASS": "django_redis.client.DefaultClient",
+#             "CONNECTION_POOL_KWARGS": {
+#                 "retry_attempts": 3,
+#             }
+#         }
+#     }
+# }
