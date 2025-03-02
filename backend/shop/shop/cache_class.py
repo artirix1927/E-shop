0824d074f7132344cfeac60b35b1f3b741f0e92b
Artyom Kotovskiy <mrartem1927@gmail.com>
2025-03-02 13:07:44 -0500

    Cache class rename + Loc mem cache adaptation
    renamed the redis cache class to loc mem cache
    and also adapted the delete keys by prefix
    method to work with loc mem cache since it
    doesnt have the same functionality as redis cache

---
+++ backend/shop/shop/cache_class.py
@@ -0,0 +1,37 @@
+from django.core.cache import caches
+import hashlib
+from django.utils.encoding import force_bytes
+from django.conf import settings
+
+
+class QuerysetCache:
+    def __init__(self, key_prefix: str, timeout=60):
+        self.key_prefix = key_prefix
+        self.timeout = timeout
+        # Dynamically choose cache backend
+        self.cache = caches[settings.DEFAULT_CACHE]
+
+    def get_cache_key(self, queryset):
+        query_key = hashlib.md5(force_bytes(str(queryset.query))).hexdigest()
+        return f"{self.key_prefix}:{query_key}"
+
+    def get(self, queryset):
+        cache_key = self.get_cache_key(queryset)
+        result = None
+        result = self.cache.get(cache_key)
+        if result is None:
+            result = list(queryset)
+            self.cache.set(cache_key, result, self.timeout)
+
+        return result
+
+    def clear_by_prefix(self, prefix):
+        keys = None
+        if self.cache.__class__.__name__ == 'LocMemCache':
+            keys = [key for key in self.cache._cache.keys()
+                    if key.startswith(prefix)]
+
+        if keys is None:
+            keys = self.cache.keys(f"{prefix}:*")
+
+        self.cache.delete_many(keys)
