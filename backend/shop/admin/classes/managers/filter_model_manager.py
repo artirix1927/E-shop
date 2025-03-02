da202fce1b16fd7561bdb9f41b35f736cefa55c2
Artyom Kotovskiy <mrartem1927@gmail.com>
2025-03-01 20:22:54 -0500

    Fixed a bug with empty query str
    Before, if the query str was empty ("?")
    the filtering back (for all instances)
    wouldnt work, now i just fixed that by
    returning an empty dict if the query str is "?"

--- backend/shop/admin/classes/managers/filter_model_manager.py
+++ backend/shop/admin/classes/managers/filter_model_manager.py
@@ -3,6 +3,7 @@ from django.contrib.admin.filters import FieldListFilter, AllValuesFieldListFilt
 from django.db import models
 from django.contrib.admin import ModelAdmin
 
+
 from shop.redis_cache_class import QuerysetCache
 
 
@@ -65,17 +66,22 @@ class FilterModelManager(BasicManager):
         return serialized_filters
 
     def get_query_params_by_string(self, query_string: str):
+        if not query_string or query_string == "?":
+            return {}  # Return an empty dictionary for an empty query string
 
         encoded_query_string = urllib.parse.unquote_plus(
             query_string.strip("?"))
-
+        print(encoded_query_string)
         query_params = dict((encoded_query_string.split("="),))
 
         return query_params
 
     def run_filter(self, model: models.Model, query_string: str):
+        print(query_string)
 
         query_params = self.get_query_params_by_string(query_string)
+        print(123)
+        print(model.objects.filter(**query_params))
         queryset = self._redis_cache.get(
             model.objects.filter(**query_params))
 
